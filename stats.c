d3007cc62857f713df8414fd94c1287fda459e30
Arpita <aabrol@stuy.edu>
2016-10-31 22:10:30 -0400

    file size, file permissions ls -l style added

--- stats.c
+++ stats.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <sys/stat.h>
 #include <unistd.h>
 #include <errno.h>
@@ -8,10 +9,38 @@
 #include <string.h> 
 #include <sys/types.h>
 
-int toBinary(int n){
+
+int toOctal(int n){
   if (n == 0) return 0;
-  if (n == 1) return 1;
-  return (n % 2) + 10 * toBinary(n/2);
+  if (n < 8) return n;
+  return (n % 8) + 10 * toOctal(n/8);
+}
+
+void lsStyle( int n ) {
+  n = toOctal(n);
+  int three = n%10;
+  int two = n%100/10;
+  int one = n%1000/100;
+  char arr[10] = "----------";
+  if( one >= 4 )
+    arr[1] = 'r';
+  if( one >= 6 )
+    arr[2] = 'w';
+  if( one == 7 )
+    arr[3] = 'x';
+  if( two >= 4 )
+    arr[4] = 'r';
+  if( two >= 6 )
+    arr[5] = 'w';
+  if( two == 7 )
+    arr[6] = 'x';
+  if( three >= 4 )
+    arr[7] = 'r';
+  if( three >= 6 )
+    arr[8] = 'w';
+  if( three == 7 )
+    arr[9] = 'x';
+  printf( "%s\n", arr);
 }
 
 
@@ -25,9 +54,23 @@ void printStats(char * file){
 
   if(stat(file, filestruct) == 0){
     printf("\n=====Stats of %s=====\n", file);
-    printf("size: %lld\n", filestruct->st_size);
+    printf("size: %lld B\n", filestruct->st_size);
     printf("mode: %o\n", filestruct->st_mode);
     printf("last access: %s\n", ctime(&(filestruct->st_atime)));
+
+    printf("File permissions (ls -l style):\n");
+    printf( (S_ISDIR(filestruct->st_mode)) ? "d" : "-");
+    printf( (filestruct->st_mode & S_IRUSR) ? "r" : "-");
+    printf( (filestruct->st_mode & S_IWUSR) ? "w" : "-");
+    printf( (filestruct->st_mode & S_IXUSR) ? "x" : "-");
+    printf( (filestruct->st_mode & S_IRGRP) ? "r" : "-");
+    printf( (filestruct->st_mode & S_IWGRP) ? "w" : "-");
+    printf( (filestruct->st_mode & S_IXGRP) ? "x" : "-");
+    printf( (filestruct->st_mode & S_IROTH) ? "r" : "-");
+    printf( (filestruct->st_mode & S_IWOTH) ? "w" : "-");
+    printf( (filestruct->st_mode & S_IXOTH) ? "x" : "-");
+    printf( "\n\nFile permissions via octals:\n");
+    lsStyle( filestruct->st_mode );
   }
   else
     printf("error %d: %s\n", errno, strerror(errno));
@@ -37,11 +80,6 @@ void printStats(char * file){
 
 int main(){
   printStats("test.txt");
-  
-  int a = toBinary(6);
-  int b = toBinary(4);
-  printf("%d\n", a);
-  printf("%d\n", b);
 
   return 0;
 }
